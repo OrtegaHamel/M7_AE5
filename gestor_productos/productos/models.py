b6262940396c13bb4655336d2802874e2bd1a549
Ortega Hamel <ortega.hamel@gmail.com>
2025-10-25 20:17:15 -0300

    Creación aplicación y primer modelo para consultas ORM

---
+++ gestor_productos/productos/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Modelo Producto
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=5, decimal_places=2)
+    disponible = models.BooleanField()
